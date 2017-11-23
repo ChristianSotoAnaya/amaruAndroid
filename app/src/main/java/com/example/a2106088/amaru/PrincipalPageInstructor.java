05357ab3e822a7918f783108f856add4060e374e
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-11-23 12:01:23 -0500

    arreglo principal instructor

--- app/src/main/java/com/example/a2106088/amaru/PrincipalPageInstructor.java
+++ app/src/main/java/com/example/a2106088/amaru/PrincipalPageInstructor.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.PermissionChecker;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,11 +16,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a2106088.amaru.entity.Clase;
+import com.example.a2106088.amaru.entity.CustomListAdapter;
 import com.example.a2106088.amaru.entity.Group;
 import com.example.a2106088.amaru.entity.User;
 import com.example.a2106088.amaru.model.NetworkException;
@@ -35,11 +40,14 @@ public class PrincipalPageInstructor extends AppCompatActivity
     SharedPreferences infousuario;
     TextView username;
     String usuario;
-    Button[] btnWord = new Button[10];
-    LinearLayout LayoutPendientes;
+    ListView lista;
+    String[] itemname ;
+    String[] descr;
+    String[] imgid;
     User user;
     RetrofitNetwork rfn;
     List<Group> grupos;
+    List<Clase> clases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,13 +79,16 @@ public class PrincipalPageInstructor extends AppCompatActivity
         Bundle memoria = anterior.getExtras();
         user= (User) memoria.getSerializable("usuario");
         usuario = user.getUsername();
+        clases=user.getClases();
 
         test();
 
     }
 
     private void test() {
-        LayoutPendientes = (LinearLayout) findViewById(R.id.LayoutPendientes);
+
+
+        /*LayoutPendientes = (LinearLayout) findViewById(R.id.LayoutPendientes);
         List<Clase> clases= user.getClases();
         for (int i=0;i<clases.size();i++){
             btnWord[i] = new Button(this);
@@ -87,10 +98,40 @@ public class PrincipalPageInstructor extends AppCompatActivity
             btnWord[i].setText(clases.get(i).getNombregrupo()+" - "+clases.get(i).getFecha()+" - "+clases.get(i).getHour());
             btnWord[i].setOnClickListener(btnClicked);
             LayoutPendientes.addView(btnWord[i]);
+        }*/
+        itemname = new String[clases.size()];
+
+        descr= new String[clases.size()];
+        imgid= new String[clases.size()];
+
+        for (int i=0;i<clases.size();i++){
+            itemname[i]=clases.get(i).getNombregrupo();
+            descr[i]="Fecha: "+clases.get(i).getFecha()+ " Hora: "+clases.get(i).getHour()+ "\nLugar: "+clases.get(i).getPlace()+ " Num Inscritos: "+clases.get(i).getNuminscritos();
+            imgid[i]="https://cdn4.iconfinder.com/data/icons/date-and-time-3/32/109-01-512.png";
         }
 
+        CustomListAdapter adapter=new CustomListAdapter(PrincipalPageInstructor.this, itemname, imgid,descr);
+        lista=(ListView) findViewById(R.id.listaaa3);
+        lista.setAdapter(adapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                Intent intento=new Intent(PrincipalPageInstructor.this,Grupo.class);
+                Bundle datosExtra = new Bundle();
+                datosExtra.putString("username",usuario);
+                intento.putExtras(datosExtra);
+                startActivity(intento);
+
+                //String Slecteditem= itemname[+position];
+                //Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();}
+        }});
 
     }
+
+/*
 
     View.OnClickListener btnClicked = new View.OnClickListener() {
         @Override
@@ -102,7 +143,7 @@ public class PrincipalPageInstructor extends AppCompatActivity
             intento.putExtras(datosExtra);
             startActivity(intento);
         }
-    };
+    };*/
 
     @Override
     public void onBackPressed() {
