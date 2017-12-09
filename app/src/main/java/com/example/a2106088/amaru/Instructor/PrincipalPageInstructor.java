a2338c3d3726dac2f9a75d11470832efdd7923b4
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-12-09 11:48:39 -0500

    on back

--- app/src/main/java/com/example/a2106088/amaru/Instructor/PrincipalPageInstructor.java
+++ app/src/main/java/com/example/a2106088/amaru/Instructor/PrincipalPageInstructor.java
@@ -177,11 +177,11 @@ public class PrincipalPageInstructor extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        //if (drawer.isDrawerOpen(GravityCompat.START)) {
-        //    drawer.closeDrawer(GravityCompat.START);
-        //} else {
-        //    super.onBackPressed(); }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed(); }
     }
 
     @Override
