ced1eedd068f5483a44e95c17e47ef4e6eaaf19e
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-12-09 19:25:20 -0500

    correcciones

--- app/src/main/java/com/example/a2106088/amaru/Usuario/UserListas.java
+++ app/src/main/java/com/example/a2106088/amaru/Usuario/UserListas.java
@@ -193,7 +193,7 @@ public class UserListas extends AppCompatActivity
                                                 Intent intento=new Intent(UserListas.this,ActivitySelectedGroupAmaru.class);
                                                 Bundle datosExtra = new Bundle();
                                                 datosExtra.putSerializable("grupo",response2);
-                                                datosExtra.putString("username",usuario);
+                                                datosExtra.putString("usuario",usuario);
                                                 datosExtra.putSerializable("user",response);
                                                 intento.putExtras(datosExtra);
                                                 startActivity(intento);
